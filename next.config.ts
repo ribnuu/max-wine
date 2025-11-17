3238f48bb0567abf20d4fdd208ee40663e9255bb
Mak Wines <mak@makwines.co.uk>
2025-11-17 21:27:02 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
+  allowedDevOrigins: ["http://localhost:3000", "http://172.17.64.1:3000"],
   /* config options here */
 };
 
